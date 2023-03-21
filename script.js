496d3ddbf386235ff1da61b7254837e46875b309
Patryk Frejek <freju13@gmail.com>
2023-03-21 13:11:07 +0100

    header animation and layout

--- script.js
+++ script.js
@@ -3,14 +3,37 @@ window.onload = init;
 const toggle = document.querySelector(".btn-menu");
 const navbar = document.querySelector(".navigation__list");
 const header = document.querySelector(".header");
+var left = document.getElementsByClassName("header__split")[0];
+var right = document.getElementsByClassName("header__split")[1];
+var split = document.getElementsByClassName("header__split");
+
 function init() {
 
-	toggle.onclick = () => {
-		navbar.classList.toggle("active");
-		header.classList.toggle("active");
-		console.log("testt");
-	};
-	
+	// toggle.onclick = () => {
+	// 	navbar.classList.toggle("active");
+	// 	header.classList.toggle("active");
+	// 	console.log("testt");
+	// };
+		left.onmouseenter = () => {
+			left.style.width = "75%";
+			right.style.width = "25%";
+			right.style.left = "75%";
+		};
+		right.onmouseenter = () => {
+			right.style.width = "75%";
+			left.style.width = "25%";
+			right.style.left = "25%";
+		};
+		left.onmouseleave = () => {
+			right.style.width = "50%";
+			left.style.width = "50%";
+			right.style.left = "50%";
+		};
+		right.onmouseleave = () => {
+			right.style.width = "50%";
+			left.style.width = "50%";
+			right.style.left = "50%";
+		};
 }
 
 
