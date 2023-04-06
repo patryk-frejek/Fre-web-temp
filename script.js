224df0072a426fcccbc1c8597c5a77c936cadfd7
Patryk Frejek <freju13@gmail.com>
2023-04-06 06:39:17 +0200

    response fix of form element JS func

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 window.onload = init;
+window.onresize = () => {
+	autoTopNavi();
+	formSizeChange();
+};
 
-
-window.onresize = autoTopNavi;
 const toggle = document.querySelector(".btn-menu");
 const navbar = document.querySelector(".navigation__list");
 var navigation = document.querySelector(".navigation");
@@ -13,15 +15,16 @@ const header = document.querySelector(".header");
 var left = document.getElementsByClassName("header__split")[0];
 var right = document.getElementsByClassName("header__split")[1];
 var split = document.getElementsByClassName("header__split");
+const form = document.querySelector(".form");
 
 function init() {
 	autoTopNavi();
-
+	formSizeChange();
 	toggle.onclick = () => {
 		navbar.classList.toggle("active");
 		toggle.firstChild.classList.toggle("fa-xmark");
 		toggle.firstChild.classList.toggle("fa-bars");
-		
+
 		navigation.classList.toggle("active");
 		console.log("test");
 	};
@@ -54,8 +57,14 @@ function init() {
 
 function autoTopNavi() {
 	if (window.innerWidth <= 600) {
-		navigation.style.top = contactBar.clientHeight + "px";;
+		navigation.style.top = contactBar.clientHeight + "px";
 	} else {
-		navigation.style.top = 0 + "px";;
+		navigation.style.top = 0 + "px";
+	}
+}
+function formSizeChange() {
+	if (window.innerWidth <= 600) {
+		let width = window.innerWidth - 30 + "";
+		form.setAttribute("width", `${width}`);
 	}
 }
